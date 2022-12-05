fa6d30ce48dfbd624b6a2c1469f9b4fcaf1cf68a
Mudassir104 <mudassirameen104@gmail.com>
2022-12-05 17:55:19 +0500

    Day 1

---
+++ day1.py
@@ -0,0 +1,12 @@
+# This is day 1.
+
+# In that day I will make a program that will generate a bend name
+
+# first it will take city name
+cityName = input("Enter your city name: ")
+
+# Then it will take your pet name
+petName = input("Enter your pet name: ")
+
+#After that it will generate your bend name
+print("Your bend name could be " + cityName + " " + petName)
